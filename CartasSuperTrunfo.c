2187bfff283597d97b024d95d7ea47a0b9919b0d
xribeirox <avancedesignstudio@gmail.com>
2025-01-15 21:54:21 -0300

    inserido variaveis e entrada/saida de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
 
-    return 0;
+    printf("Cadastre sua Cidade com as Informações solicitadas abaixo.\n")
+
+    printf("Qual o nome da sua Cidade?\n")
+    scanf("%s", &nome);
+
+    printf("Qual a População de sua Cidade?\n");
+    scanf("%d", &populacao);
+    
+    printf("Qual a Área em KM² de sua Cidade?\n");
+    scanf("%f", &area);
+
+    printf("Qual o PIB em R$ de sua Cidade?\n");
+    scanf("%f", &pib);
+
+    printf("Quantos Pontos Turísticos sua Cidade possui?\n");
+    scanf("%f", &pontosturisticos);
+
+    printf("Você cadastrou a cidade: %s, Confira o cadastro abaixo!\n", nome);
+    printf("Cidade Nome: %s\n", nome);
+    printf("População: %d\n", populacao);
+    printf("Area KM²: %f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Número de Pontos Túristicos: %d\n", pontosturisticos);
+
+    printf("\nCidade Cadastrada com Sucesso!\n");
+    
+    return 0;
 }
